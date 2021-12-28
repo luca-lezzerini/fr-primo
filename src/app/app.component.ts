fff25a26df4ffaa1734eceaf238b8610d3c15ff7
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-12-28 19:46:26 +0100

    Bottoni contatori funzionanti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primo';
+  // (variabile) proprietà contatore
+  contatore: number = 0;
+
+  incrementa(){
+    console.log("Siamo nella funzione Incrementa con contatore = ", this.contatore);
+    this.contatore = this.contatore + 1;
+  }
+
+  decrementa(){
+    console.log("Siamo nella funzione Decrementa con contatore = ", this.contatore);
+    this.contatore = this.contatore - 1;
+  }
+
 }
